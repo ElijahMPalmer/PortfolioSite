cb3bf9d311a44b6746eaed378bbf73375748090f
ElijahMPalmer <palmerelijah@rocketmail.com>
2022-03-07 20:06:22 -0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    navigation: true,
+    controlArrows: false,
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom',
+    anchors: ['section1', 'section2', 'section3']
+})
